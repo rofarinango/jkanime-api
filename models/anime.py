d8721a6e91be45b6d91f52aa370806f057920ed3
Rodrigo Farinango <47066093+rofarinango@users.noreply.github.com>
2025-05-15 10:25:54 -0600

    add: data property to make object serializable

--- models/anime.py
+++ models/anime.py
@@ -7,3 +7,13 @@ class Anime:
     image: Optional[str] = None
     synopsis: Optional[str] = None
     type: Optional[str] = None
+
+    @property
+    def data(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "image": self.image,
+            "synopsis": self.synopsis,
+            "type": self.type
+        }
