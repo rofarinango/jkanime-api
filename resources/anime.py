b9a2e5e216cdac539c8e56070a6ef70a7a40d3bc
Rodrigo Farinango <47066093+rofarinango@users.noreply.github.com>
2025-05-15 10:26:36 -0600

    add: implement GET method, gets anime by query and page

--- resources/anime.py
+++ resources/anime.py
@@ -1,9 +1,20 @@
 from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
-from main import JKAnime
+from services.jkanime_service import JKAnimeService
 from models.anime import Anime
 
 class AnimeResource(Resource):
-    def get(seld, query, page):
-        pass
+    def __init__(self):
+        self.service = JKAnimeService()
+
+    def get(self, query: str, page: int):
+        try:
+            search_results = self.service.search_anime(query, page)
+            if search_results:
+                return {
+                    'data': [anime.data for anime in search_results]
+                }
+            return {'message': 'animes not found'}, HTTPStatus.NOT_FOUND
+        except Exception as e:
+            return {'error': str(e)}, HTTPStatus.INTERNAL_SERVER_ERROR
